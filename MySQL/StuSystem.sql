6106a0a7a689e0507254f92003bf70b339d5d992
Muieay <101854209+Muieay@users.noreply.github.com>
2022-06-11 14:19:09 +0800

    Add files via upload

---
+++ MySQL/StuSystem.sql
@@ -0,0 +1,9 @@
+USE student;
+SELECT * FROM stu2;
+INSERT INTO stu2 
+VALUES(NULL,'欧阳娜娜','女',16,'5201314'),
+	(NULL,'迪丽热巴','女',31,'1231231'),
+	 (NULL,'欧阳娜娜','女',18,'5201314'),
+	 (NULL,'张三','男',19,'23123123');
+	 
+	 
